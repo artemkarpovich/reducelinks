39114299d2c5a828b1b4f43a6c46a5ee352e6b26
Artem Karpovich <karpartem94@gmail.com>
2016-06-03 22:55:07 +0300

    implemetne redirect on shortLink

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import config from './config';
-import router from './routes/api';
+import apiRouter from './routes/api';
+import redirectRouter from './routes/redirect';
 
 const app = express();
 
@@ -11,7 +12,8 @@ app.use(bodyParser.json());
 
 mongoose.connect(config.database);
 
-app.use('/', router);
+app.use('/redirect', redirectRouter);
+app.use('/api', apiRouter);
 
 app.listen(config.port, function() {
   console.log('server is running on port:' + config.port);
